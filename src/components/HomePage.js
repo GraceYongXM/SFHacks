f95b0ff3d59dd449c9d22f47e5979277bf97cf60
Grace Yong <grace.yong.xue.min@gmail.com>
2024-04-07 02:37:28 -0700

    updated join group chat mechanism

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { db, auth } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
@@ -32,26 +32,47 @@ const HomePage = () => {
       // Check if the chat room exists in Firebase Firestore
       const chatRoomRef = doc(db, "Chatrooms", groupChatName);
       const chatRoomSnapshot = await getDoc(chatRoomRef);
+      console.log("chatRoomSnapshot", chatRoomSnapshot);
 
       // If the chat room exists, check if the user exists in that chat room
-      const userId = getUserEmail();
-      if (chatRoomSnapshot.exists) {
-        const chatRoomData = chatRoomSnapshot.data();
-        if (
-          chatRoomData["Participants"] &&
-          chatRoomData["Participants"].includes(userId)
-        ) {
+      const userEmail = getUserEmail();
+      const chatRoomData = chatRoomSnapshot.data();
+      if (chatRoomData) {
+        const participants = chatRoomData["Participants"] || [];
+        if (participants.includes(userEmail)) {
           // User exists in the chat room
           setParticipants(true);
           console.log("User exists in the chat room:", groupChatName);
         } else {
-          // User does not exist in the chat room
-          setParticipants(false);
-          console.log("User does not exist in the chat room:", groupChatName);
+          // User does not exist in the chat room yet
+          // Add user in
+          participants.push({
+            email: userEmail,
+            has_sent_messages: false,
+            messages: [],
+            username: auth.currentUser.displayName,
+          });
+          await updateDoc(chatRoomRef, {
+            Participants: participants,
+          });
+          console.log("User added to the chat room:", groupChatName);
+          setParticipants(true);
         }
       } else {
         // Chat room does not exist
-        console.log("Chat room does not exist:", groupChatName);
+        // Create chat room
+        await setDoc(chatRoomRef, {
+          Participants: [
+            {
+              email: userEmail,
+              has_sent_message: false,
+              messages: [],
+              username: auth.currentUser.displayName,
+            },
+          ],
+        });
+        console.log("Chat room created:", groupChatName);
+        setParticipants(true);
       }
     } catch (error) {
       console.error("Error checking chat room existence:", error);
